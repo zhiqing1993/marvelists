dfa7ff53f6a013bdbc5209dcd92cee16173fb501
Charlene Wroblewski <cwroblew@gmail.com>
2014-04-26 16:04:03 -0500

    Modified for my keys

--- script/app.js
+++ script/app.js
@@ -3,8 +3,8 @@
 var marvelApp = angular.module('marvelApp', ['ngResource', 'ngRoute', 'marvelApp.controllers', 'marvelApp.services']);
 
 marvelApp.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when('/characters',    { templateUrl: '/view/characters.html', controller: 'CharactersListCtrl' });
-  $routeProvider.when('/character/:id', { templateUrl: '/view/character.html',  controller: 'CharacterCtrl' });  
+  $routeProvider.when('/characters',    { templateUrl: '/marvelists/view/characters.html', controller: 'CharactersListCtrl' });
+  $routeProvider.when('/character/:id', { templateUrl: '/marvelists/view/character.html',  controller: 'CharacterCtrl' });  
 }]);
 
 marvelApp.config(['$locationProvider', function ($locationProvider) {
--- script/services/marvel_service.js
+++ script/services/marvel_service.js
@@ -1,5 +1,5 @@
 marvelAppServices.factory('MarvelService', function ($resource) {
-  return $resource('http://gateway.marvel.com/v1/public/characters/:id', { id: '@id', "apikey": api.key }, {
+  return $resource('http://gateway.marvel.com/v1/public/characters/:id', { id: '@id', "ts": api.ts, "apikey": api.key, "hash": api.hash }, {
     update: { method: 'PUT' }
   });
 });
